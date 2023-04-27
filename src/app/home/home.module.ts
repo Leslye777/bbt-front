6363bac06f4730b3c84d1551daad2380e740a880
Leslye <leslimdf@hotmail.com>
2023-04-27 05:41:15 -0300

    Ok

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeService } from './home.service';
-
+import { HomeRoutingModule } from './home-routing.module';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-
   ],
   imports: [
     CommonModule,
+    HomeRoutingModule,
+
 
   ],
   providers:[HomeService]
